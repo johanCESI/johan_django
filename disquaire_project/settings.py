e1063e43d57890ce7e120aee4fc633cf9f420dd8
Lecuyer johan <johan.lecuyer4@gmail.com>
2019-05-26 19:21:52 +0200

    modify allowed host

--- disquaire_project/settings.py
+++ disquaire_project/settings.py
@@ -148,7 +148,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
-    ALLOWED_HOSTS = ['johan_disquaire.herokuapp.com']
+    ALLOWED_HOSTS = ['disquairejohan.herokuapp.com']
 else:
     DEBUG = True
     ALLOWED_HOSTS = ['127.0.0.1']
